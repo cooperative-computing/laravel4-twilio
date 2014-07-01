77e72c14810aa419fe14682af0ffb0354cea64e3
Waqas Ashraf <waqas.ashraf@cooperativecomputing.net>
2014-07-01 11:30:47 +0500

    Initial Commit - Created base structure for the Laravel Package

---
+++ src/CooperativeComputing/Laravel4Twilio/Laravel4TwilioServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace CooperativeComputing\Laravel4Twilio;
+
+use Illuminate\Support\ServiceProvider;
+
+class Laravel4TwilioServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('cooperative-computing/laravel4-twilio');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
