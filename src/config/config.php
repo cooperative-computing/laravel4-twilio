9e2eda31b6b89ff8f0fff804556f09c36b902126
Waqas Ashraf <waqas.ashraf@cooperativecomputing.net>
2014-07-01 12:04:41 +0500

    Added package configuration file

---
+++ src/config/config.php
@@ -0,0 +1,37 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Twilio Account SID
+    |--------------------------------------------------------------------------
+    |
+    | The 34 character string that uniquely identifies the Twilio account. 
+    | It can be found right under the main navigation after logging into Twilio.
+    |
+    */
+    'sid'   => '',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Twilio Auth Token
+    |--------------------------------------------------------------------------
+    |
+    | The authorization token for the specific Twilio account.
+    | This token should be kept a secret.
+    |
+    */
+    'token' => '',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Twilio From Phone Number
+    |--------------------------------------------------------------------------
+    |
+    | Enter your registered Twilio Phone Number from where all SMS and Calls
+    | will be sent.
+    |
+    */
+    'from'  => ''
+);
