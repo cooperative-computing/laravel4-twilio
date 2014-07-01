6801cbed9de4e8b6072ae22a78f1a0629ba7386e
Waqas Ashraf <waqas.ashraf@cooperativecomputing.net>
2014-07-01 11:34:07 +0500

    Updated Service Provider codebase to PSR-2 Coding Standards

--- src/CooperativeComputing/Laravel4Twilio/Laravel4TwilioServiceProvider.php
+++ src/CooperativeComputing/Laravel4Twilio/Laravel4TwilioServiceProvider.php
@@ -2,43 +2,43 @@
 
 use Illuminate\Support\ServiceProvider;
 
-class Laravel4TwilioServiceProvider extends ServiceProvider {
+class Laravel4TwilioServiceProvider extends ServiceProvider
+{
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->package('cooperative-computing/laravel4-twilio');
-	}
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('cooperative-computing/laravel4-twilio');
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		//
-	}
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
-	}
-
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array();
+    }
 }
